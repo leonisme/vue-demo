de70a93f8509ae0d9941ad36c08903ab92c07bad
Leon Lu <leonchina@aliyun.com>
2017-01-26 22:07:00 +0800

    Components

--- Components/components.js
+++ Components/components.js
@@ -76,7 +76,7 @@ Vue.component('button-counter', {
         }
     },
 })
-new Vue({
+var app6 = new Vue({
     el: '#app-6',
     data: {
         total: 0
@@ -84,6 +84,77 @@ new Vue({
     methods: {
         incrementTotal: function () {
             this.total += 1
+        }
+    }
+})
+
+
+//Form Input Components using Custom Events
+
+Vue.component('currency-input', {
+    template: '\
+    <div>\
+      <label v-if="label">{{ label }}</label>\
+      $\
+      <input\
+        ref="input"\
+        v-bind:value="value"\
+        v-on:input="updateValue($event.target.value)"\
+        v-on:focus="selectAll"\
+        v-on:blur="formatValue"\
+      >\
+    </div>\
+  ',
+    props: {
+        value: {
+            type: Number,
+            default: 0
+        },
+        label: {
+            type: String,
+            default: ''
+        }
+    },
+    mounted: function () {
+        this.formatValue()
+    },
+    methods: {
+        updateValue: function (value) {
+            var result = currencyValidator.parse(value, this.value)
+            if (result.warning) {
+                this.$refs.input.value = result.value
+            }
+            this.$emit('input', result.value)
+        },
+        formatValue: function () {
+            this.$refs.input.value = currencyValidator.format(this.value)
+        },
+        selectAll: function (event) {
+            // Workaround for Safari bug
+            // http://stackoverflow.com/questions/1269722/selecting-text-on-focus-using-jquery-not-working-in-safari-and-chrome
+            setTimeout(function () {
+                event.target.select()
+            }, 0)
+        }
+    }
+})
+
+var app7 = new Vue({
+    el: '#app-7',
+    data: {
+        price: 0,
+        shipping: 0,
+        handling: 0,
+        discount: 0
+    },
+    computed: {
+        total: function () {
+            return ((
+                this.price * 100 +
+                this.shipping * 100 +
+                this.handling * 100 -
+                this.discount * 100
+            ) / 100).toFixed(2)
         }
     }
 })
